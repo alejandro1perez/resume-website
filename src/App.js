52726146307a5756b40e0c3af1cc115d402e6f5a
Alejandro Perez <perez.alejandro1221@gmail.com>
2021-10-15 20:36:23 -0400

    Education

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
+import Education from "./components/Education";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <AboutMe />
       <Skills />
+      <Education />
     </>
   );
 }
---
+++ src/components/Education.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+const Education = () => {
+    return (
+        <div id="education" className="education">
+            <h1 className="py-5">Education</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-4">
+                            <div className="box">
+                                <div className="circle">
+                                    <FontAwesomeIcon className="icon" icon={faGraduationCap} size="2x" />
+                                </div>
+                                <h3>Colegio San Antonio, Rio Piedras, PR</h3>
+                                <p>Highschool Degree, 2016</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-sm-6">
+                            <div className="box">
+                            <div className="circle">
+                                <FontAwesomeIcon className="icon" icon={faGraduationCap} size="2x" />
+                            </div>
+                                <h3>Barry University, Miami, FL</h3>
+                                <p>Bachelor in Computer Science with a Math Minor, 2020</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        
+    )
+}
+
+export default Education
